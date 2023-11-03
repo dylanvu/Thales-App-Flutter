d82d657e6a1a6408628fd679fb054e9a73c55da9
Dylan Vu <dylanvu9@gmail.com>
2023-11-02 17:26:32 -0700

    move sensor into stateful widget

--- lib/device_search.dart
+++ lib/device_search.dart
@@ -1,5 +1,6 @@
 /* Page to show while looking for device */
 
+import 'package:ecg_app/components/sensor_graph.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
@@ -44,44 +45,7 @@ class _DataPageState extends State<DataPage> {
             Row(
               mainAxisAlignment: MainAxisAlignment.center,
               children: <Widget>[
-                SfCartesianChart(
-                  plotAreaBorderColor: Colors.white,
-                  plotAreaBorderWidth: 5,
-                  primaryXAxis: CategoryAxis(
-                    axisLine: const AxisLine(color: Colors.white, width: 3),
-                    majorGridLines: const MajorGridLines(color: Colors.white),
-                    labelStyle:
-                        const TextStyle(color: Colors.white, fontSize: 15),
-                  ),
-                  primaryYAxis: NumericAxis(
-                    axisLine: const AxisLine(color: Colors.white, width: 3),
-                    majorGridLines: const MajorGridLines(color: Colors.white),
-                    labelStyle:
-                        const TextStyle(color: Colors.white, fontSize: 15),
-                  ),
-                  title: ChartTitle(
-                      text: widget.title,
-                      textStyle:
-                          const TextStyle(fontSize: 20, color: Colors.white)),
-                  series: <LineSeries<HeartRateData, String>>[
-                    LineSeries<HeartRateData, String>(
-                      // Bind data source
-                      dataSource: <HeartRateData>[
-                        HeartRateData('0 min', 62),
-                        HeartRateData('5 min', 65),
-                        HeartRateData('10 min', 67),
-                        HeartRateData('15 min', 66),
-                        HeartRateData('20 min', 68)
-                      ],
-                      xValueMapper: (HeartRateData person, _) => person.time,
-                      yValueMapper: (HeartRateData person, _) => person.rate,
-                      dataLabelSettings: const DataLabelSettings(
-                          isVisible: true,
-                          textStyle:
-                              TextStyle(fontSize: 15, color: Colors.white)),
-                    )
-                  ],
-                ),
+                SensorGraph(title: widget.title),
                 const SizedBox(width: 300),
                 Icon(
                   widget.icon,
@@ -95,10 +59,4 @@ class _DataPageState extends State<DataPage> {
       ),
     );
   }
-}
-
-class HeartRateData {
-  HeartRateData(this.time, this.rate);
-  final String time;
-  final double rate;
 }
